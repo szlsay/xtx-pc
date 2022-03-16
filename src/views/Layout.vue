135a995fb82b2383b4ddd6a5766ff66cfd9d064b
小沈曰 <409178030@qq.com>
2022-03-16 15:06:07 +0800

    11

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -4,7 +4,7 @@
 <!-- 头部组件 -->
 <AppHeader />
 <!-- 吸顶头部 -->
-<!-- <AppHeaderSticky /> -->
+<AppHeaderSticky />
 <!-- 内容容器 -->
 <div class="app-body">
   <!-- 二级路由 -->
@@ -18,13 +18,15 @@
 import AppNavbar from '@/components/app-navbar.vue'
 import AppHeader from '@/components/app-header.vue'
 import AppFooter from '@/components/app-footer.vue'
+import AppHeaderSticky from '@/components/app-header-sticky'
 import { useStore } from 'vuex'
 export default {
   name: 'LayoutPage',
   components: {
     AppNavbar,
     AppHeader,
-    AppFooter
+    AppFooter,
+    AppHeaderSticky
   },
   // 获取下分类数据
   setup () {
