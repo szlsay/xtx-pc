c346923c4c774fc3060ab394aacc18af5816f420
小沈曰 <409178030@qq.com>
2022-03-16 08:20:16 +0800

    05

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
+<button @click="requestHandle">request</button>
   <router-view></router-view>
 </template>
+
+<script>
+import request from '@/utils/request'
+export default {
+  setup () {
+    const requestHandle = () => {
+      request('/member/profile', 'get', { a: 10 })
+    }
+    return {
+      requestHandle
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
+const Layout = () => import('@/views/Layout')
+
 const routes = [
+  {
+    path: '/',
+    name: 'layout',
+    component: Layout
+  }, {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login')
+  }
 ]
 
 const router = createRouter({
