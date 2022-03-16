418302d758f39b93effb05f969ce3e4b7d2815b6
小沈曰 <409178030@qq.com>
2022-03-17 07:52:14 +0800

    03

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -1,15 +1,14 @@
-
 // 扩展vue原有的功能：全局组件，自定义指令，挂载原型方法，注意：没有全局过滤器。
 // 这就是插件
 // vue2.0插件写法要素：导出一个对象，有install函数，默认传入了Vue构造函数，Vue基础之上扩展
 // vue3.0插件写法要素：导出一个对象，有install函数，默认传入了app应用实例，app基础之上扩展
 
 import defaultImg from '@/assets/images/200.png'
-import XtxSkeleton from './xtx-skeleton.vue'
-import XtxCarousel from './xtx-carousel.vue'
-import XtxMore from './xtx-more.vue'
-import XtxBread from './xtx-bread.vue'
-import XtxBreadItem from './xtx-bread-item.vue'
+// import XtxSkeleton from './xtx-skeleton.vue'
+// import XtxCarousel from './xtx-carousel.vue'
+// import XtxMore from './xtx-more.vue'
+// import XtxBread from './xtx-bread.vue'
+// import XtxBreadItem from './xtx-bread-item.vue'
 
 // 使用 `require` 提供的函数 `context`  加载某一个目录下的所有 `.vue` 后缀的文件。
 // 然后 `context` 函数会返回一个导入函数 `importFn`
@@ -20,31 +19,31 @@ import XtxBreadItem from './xtx-bread-item.vue'
 // import Message from './Message'
 // import Confirm from './Confirm'
 
-// // context(目录路径，是否加载子目录，加载文件的匹配正则)
-// const importFn = require.context('./', false, /\.vue$/)
+// context(目录路径，是否加载子目录，加载文件的匹配正则)
+const importFn = require.context('./', false, /\.vue$/)
 
 export default {
   install (app) {
     // 在app上进行扩展，app提供 component directive 函数
     // 如果要挂载原型 app.config.globalProperties 方式
-    app.component(XtxSkeleton.name, XtxSkeleton)
-    app.component(XtxCarousel.name, XtxCarousel)
-    app.component(XtxMore.name, XtxMore)
-    app.component(XtxBread.name, XtxBread)
-    app.component(XtxBreadItem.name, XtxBreadItem)
+    // app.component(XtxSkeleton.name, XtxSkeleton)
+    // app.component(XtxCarousel.name, XtxCarousel)
+    // app.component(XtxMore.name, XtxMore)
+    // app.component(XtxBread.name, XtxBread)
+    // app.component(XtxBreadItem.name, XtxBreadItem)
 
-    // // 根据keys批量注册
-    // importFn.keys().forEach(path => {
-    //   // 导入组件
-    //   const component = importFn(path).default
-    //   // 进行注册
-    //   app.component(component.name, component)
-    // })
+    // 根据keys批量注册
+    importFn.keys().forEach(path => {
+      // 导入组件
+      const component = importFn(path).default
+      // 进行注册
+      app.component(component.name, component)
+    })
 
     // 定义指令
     defineDirective(app)
 
-    // // 定义一个原型函数
+    // 定义一个原型函数
     // app.config.globalProperties.$message = Message
     // app.config.globalProperties.$confirm = Confirm
   }
